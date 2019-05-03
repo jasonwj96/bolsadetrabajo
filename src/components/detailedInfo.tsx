5be700a49bb023a1e202e411bb572a05dae6a7b2
Jason Wedderburn <JasonAlejandro507@hotmail.com>
2019-05-02 19:53:23 -0500

    Added a detailed info component

---
+++ src/components/detailedInfo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./detailedInfo.scss";
+
+const DetailedInfo = (props: any) => {
+  return (
+    <div className="detailedInfo-container">
+      <form className="form">
+        <div className="input">
+          <fieldset>
+            <legend>Dirección</legend>
+            <textarea />
+          </fieldset>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default DetailedInfo;
--- src/containers/cuentaEstudiante.tsx
+++ src/containers/cuentaEstudiante.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./cuentaEstudiante.scss";
 import Navbar from "../components/navbar";
 import PerfilUsuario from "../components/perfilUsuario";
+import DetailedInfo from "../components/detailedInfo";
 
 const CuentaEstudiante = (props: any) => {
   return (
     <div className="cuentaestudiante-container">
       <Navbar />
       <PerfilUsuario />
+      <DetailedInfo />
     </div>
   );
 };
