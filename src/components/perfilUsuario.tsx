b9719aa46fdec4fce6ab2defa31dfafbbce46bd0
Jason Wedderburn <JasonAlejandro507@hotmail.com>
2019-05-02 19:36:01 -0500

    Separated public info into a separate component

--- src/components/perfilUsuario.tsx
+++ src/components/perfilUsuario.tsx
@@ -2,6 +2,7 @@ import React from "react";
 // import profile from "../img/profile.jpg";
 import profile from "../img/profile_placeholder.png";
 import "./perfilUsuario.scss";
+import PublicInfo from "./publicInfo";
 
 const PerfilUsuario = (props: any) => {
   return (
@@ -15,37 +16,7 @@ const PerfilUsuario = (props: any) => {
           <p>Usuario desde: 30/4/19</p>
         </div>
       </div>
-      <div className="public-info">
-        <p>Información publica</p>
-        <form className="form">
-          <div className="input">
-            <fieldset>
-              <legend>Nombre</legend>
-              <input type="text" />
-            </fieldset>
-          </div>
-          <div className="input">
-            <fieldset>
-              <legend>Apellido</legend>
-              <input type="text" />
-            </fieldset>
-          </div>
-          <div className="input">
-            <fieldset>
-              <legend>Cedula</legend>
-              <input type="text" />
-            </fieldset>
-          </div>
-          <div className="input">
-            <fieldset>
-              <legend>Celular/Telefono</legend>
-              <input type="text" />
-            </fieldset>
-          </div>
-        </form>
-      </div>
-
-      <div className="info-usuario" />
+      <PublicInfo />
     </div>
   );
 };
---
+++ src/components/publicInfo.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./publicInfo.scss";
+
+const PublicInfo = (props: any) => {
+  return (
+    <div className="publicinfo-container">
+      <p>Información publica</p>
+      <form className="form">
+        <div className="input">
+          <fieldset>
+            <legend>Nombre</legend>
+            <input type="text" />
+          </fieldset>
+        </div>
+        <div className="input">
+          <fieldset>
+            <legend>Apellido</legend>
+            <input type="text" />
+          </fieldset>
+        </div>
+        <div className="input">
+          <fieldset>
+            <legend>Cedula</legend>
+            <input type="text" />
+          </fieldset>
+        </div>
+        <div className="input">
+          <fieldset>
+            <legend>Celular/Telefono</legend>
+            <input type="text" />
+          </fieldset>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PublicInfo;
